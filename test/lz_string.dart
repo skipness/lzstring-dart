5f90102f2750daedeb922701faa21720da47f427
skipness <skipness123@gmail.com>
2019-04-10 12:51:23 +0800

    Add non english characters in test case

--- test/lz_string.dart
+++ test/lz_string.dart
@@ -2,7 +2,7 @@ import 'package:test/test.dart';
 import 'package:lzstring/lzstring.dart';
 
 void main() {
-  final String input = 'String for testing';
+  final String input = "測試,测试,テスト,testing";
   final String normal = LZString.compress(input);
   final String base64 = LZString.compressToBase64(input);
   final String utf16 = LZString.compressToUTF16(input);
@@ -12,46 +12,57 @@ void main() {
 
   group('compress data', () {
     test('to normal', () {
-      expect(normal, equals('㊁灎ॠ瘎怄̰㶘頙쓑退'));
+      expect(normal, equals('贝ꌲ蠴贫梫텨挌觐젉蘰׀ꘆ煁Ⰷ恳 '));
     });
 
     test('to base 64', () {
-      expect(base64, equals('MoFwTglgdg5gBAMwPZjiApgZxNGQ'));
+      expect(base64, equals('jR2jMog0jStoq9FoYwyJ0MgJhjAFwKYGcUEsB2BzIA=='));
     });
 
     test('to utf 16', () {
-      expect(utf16, equals('ᥠ尳䅌ހ猠ာ悛ᤂŬڑᩒ  '));
+      expect(utf16, equals('䚮棬儦䣲孥⽥僦಩梄ʁ䘠尪こ䔤堮悓ဠ '));
     });
 
     test('to encoded uri component', () {
-      expect(encodedUriComponent, equals('MoFwTglgdg5gBAMwPZjiApgZxNGQ'));
+      expect(encodedUriComponent,
+          equals('jR2jMog0jStoq9FoYwyJ0MgJhjAFwKYGcUEsB2BzIA'));
     });
 
     test('to uint8 array', () {
       expect(
           uint8Array.toList(),
           equals([
+            141,
+            29,
+            163,
             50,
-            129,
-            112,
-            78,
-            9,
-            96,
-            118,
-            14,
-            96,
-            4,
-            3,
-            48,
-            61,
-            152,
-            226,
-            2,
-            152,
-            25,
-            196,
+            136,
+            52,
+            141,
+            43,
+            104,
+            171,
             209,
-            144,
+            104,
+            99,
+            12,
+            137,
+            208,
+            200,
+            9,
+            134,
+            48,
+            5,
+            192,
+            166,
+            6,
+            113,
+            65,
+            44,
+            7,
+            96,
+            115,
+            32,
             0
           ]));
     });
