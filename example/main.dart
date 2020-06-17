dac987fdb9a35f1fce39dcab6d0c552bb50b443e
skipness <skipness123@gmail.com>
2020-06-17 15:16:55 +0800

    Update example

--- example/main.dart
+++ example/main.dart
@@ -1,12 +1,12 @@
 import 'package:lzstring/lzstring.dart';
 
-void main() { 
+void main() async { 
   String input = "Dart implemntation of lz-string";
   print('Input: $input');
-  print('Compressed String: ${LZString.compress(input)}');
-  print('Compressed Base 64 String: ${LZString.compressToBase64(input)}');
-  print('Compressed UTF16 String: ${LZString.compressToUTF16(input)}');
+  print('Compressed String: ${await LZString.compress(input)}');
+  print('Compressed Base 64 String: ${await LZString.compressToBase64(input)}');
+  print('Compressed UTF16 String: ${await LZString.compressToUTF16(input)}');
   print(
-      'Compressed Encoded URI Component: ${LZString.compressToEncodedURIComponent(input)}');
-  print('Compressed Uint8 Array: ${LZString.compressToUint8Array(input)}');
+      'Compressed Encoded URI Component: ${await LZString.compressToEncodedURIComponent(input)}');
+  print('Compressed Uint8 Array: ${await LZString.compressToUint8Array(input)}');
 }
